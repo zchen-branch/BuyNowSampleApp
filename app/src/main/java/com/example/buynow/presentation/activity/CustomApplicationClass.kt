1cfa225f1e3f5e73865b7ad626c9d9c819e1331b
zchen-branch <87984967+zchen-branch@users.noreply.github.com>
2023-03-01 15:25:54 -0500

    Branch SDK Integration

---
+++ app/src/main/java/com/example/buynow/presentation/activity/CustomApplicationClass.kt
@@ -0,0 +1,17 @@
+package com.example.buynow.presentation.activity
+
+import android.app.Application
+import io.branch.referral.Branch
+
+
+class CustomApplicationClass : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        // Branch logging for debugging
+        Branch.enableLogging()
+
+        // Branch object initialization
+        Branch.getAutoInstance(this)
+    }
+}
